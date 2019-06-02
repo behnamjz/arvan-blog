7248ee7943d6f25586109c1137ed9890a546218d
bjaberi <behnam.ite@gmail.com>
2019-06-03 00:02:36 +0430

    created header and links

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="container col-md-12">
-      header
-  </div>
+  <header class="header">
+    <div class="container">
+      <b-navbar toggleable="lg" type="dark">
+        <b-navbar-brand href="#">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="50px" height="50px" viewBox="0 0 70 70" enable-background="new 0 0 70 70" xml:space="preserve">  <image id="image0" width="70" height="70" x="0" y="0"
+              xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEYAAABGCAQAAADbJyoPAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+          AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+          RQfjBgIWByAVyZdqAAAFf0lEQVRo3s2aT0xcRRjAf/t22aUsiwXB0hZCoCZFvYhIoklj0sSTl15k
+          axPXQyWQ9MLBA/RE0pMcTU9F1ESIpuClBz0ZTUyjSdW0F2rTWAh/lT9CYRfKLvDwsLszs8u89/bt
+          vm349jIzb97Ob773vW++N9/4DilKQoQIEqQCPwYGYGJywB4pUiRJFvOnPpcwfqqpogq/Q78Ddtgh
+          wUG5YMK8QBifi38/ZJtNtr2F8RGhjqCbWSqSYp04BQxUCEwVLxUNIoFW2CkVJkADEU37OV6nnVbO
+          UksNfuCALTZYZIZHPOCJ5p44q+wXD1NNI0ZeWweXuEijwyT/5WfucD+v1WSZeDEwPho4mdNSSTcx
+          WihcZhnjO57ltD1l1cp+rGD8nOGEUq8gRg8vugDJyn+MMsae0vKMJf0rr4cJ0JRjsm8xRFsRIFmZ
+          5ga/KfUUCzrr0cEEaKZC1IIM8GEJIFkZZ5iUqO0xfxTnKIyfZkUrzXzGax6gADyknzlFO/P5Dysf
+          xkczlaL2CqPUe4QCsEYPf4naLvO5ppz/4jYoKF2Me4oC9YzTJWqVNNhpppozotzOuNbdlSpxYop2
+          lkjoNRNQXFkLX5UFBSJ8qfiqRgJ6mAZRO8FN6sqCAlDHTeHDDE7pYMKKJq5zvmwoAOe5roxbfRRG
+          GtMFLpcVBeAyF0RZvCRZA45wOtMS4nuayw4DC7wngtN/0stnVjPSQmIWKH34HH9RhhkpEKZJ8euZ
+          0dMwYUKZ5kquljDbSQbpw0cffxbQ+6rwaSHCEqZGdIgWtTLnywhvEmXaoVc9UVGuycIY0p49WRKz
+          WjrHpEMfOVo1RhomIiL+Dlehk7NEHSyohY5MyUckDRMWFy95igLQ56AdOWI4DVMlGi56DgN9trYj
+          R6yCACHxerc6htn50k2nKI9YDLrBMLcs/6GRtsx9BqGAEjK84XrW79IrygNMM6h9KCMM2AStHWIS
+          lYYS1bW7hsmVNib4VHvFzozlqEEVppSQW+qn2yWMHDVoKPHEWQ9g0Opmw8aIZTgXMJTNjZOewLQp
+          Ri3FenmQDtdvKEGEV5GdDmbDsrf0coYKE8Ab0dlegTDHSAxMUd4v4X9U0RlrrWVvua9lqjA2mxWu
+          RGesBcLIT8ynHulFB9Np2V9+Nx0YysNZ9ARmUKsXa4e6JEr7hrIz4BSZFSLD2tWp1+YOOWoqoMA8
+          KhHEaqG0h5GjpgLsisp93MqPiv8YsdRsr+2qJ0fdDZDEzDi+aZbVj80CZNIxyoVaBmyuLospmCQN
+          UPZnf3KtG2e5ZasXOeJOOuyUb/qdMqB0216XI26nYeRW+gNmPUWZsDVdmBUWc0icTHImIZpuewbS
+          zRMHraCMlsDMflFuKXPZ9ACklz+YcIwcN5kQ5S3Ihg3bJDNf23FG+aQEbXRS6/BopHwuVsNk2m51
+          WyI/0OSBdpzEZkskLjxxkqHngAJDAiWV1ZAMrlZF6S7flB3lW+6K8lq2oG69nhZRcJDbvFpGlId8
+          IPSyLaMFNexcFYFWin7Wy4ayTr9AMVmWF1SYfVZEeY4ezyK/XInTo2QQVtRgNzcg31K8zBTXCs+7
+          FizbXGNK1DYVD8fR3MGKElLcIyaNyxNZI8Y9UdtVnoQW5pBFJdya4gqPPUN5zBVFKykW89ODR7+b
+          DlhQknhzRBWnXYpMEFVsZY+Fo6nBwtKCbzNEawkgM9zgV6XuIi0IuoTpR3xcZML0C77OSZjusugm
+          YQq6VPIJ3neZSp5jjMnSU8lpiXBKm2R/x3EvZ5FfvEyyp8Xq+EEzXbxMKy1EqKIaSLBDnFlm+Jvf
+          mdfck8h1ce5hAMI0eHAwY9XZhT6fIysbbHl1ZEVq6Fgc5pFybI45qaIeAPPj4xATE7O0A2D/A7iy
+          hIomLQ6cAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTA2LTAyVDIyOjA3OjMyKzAzOjAwpiXgxAAA
+          ACV0RVh0ZGF0ZTptb2RpZnkAMjAxOS0wNi0wMlQyMjowNzozMiswMzowMNd4WHgAAAAASUVORK5C
+          YII=" />
+          </svg>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto" right>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/">Screen</router-link>
+            <router-link to="/">Contact</router-link>
+            <router-link to="/" class="regsiter-button">Create Account</router-link>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+
+      <h2 class="blog-title">Blog Title</h2>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {
-  name: 'Haeder',
-  components: {}
-}
+export default {name: 'header'}
 </script>
