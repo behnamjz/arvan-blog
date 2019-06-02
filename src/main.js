5bb243123636587cb57c1b4b87d6df3d8746e30d
behnam <behnam_it87@yahoo.com>
2019-06-02 19:31:55 +0430

    added vue- bootstrap and globally sass files to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
 
+// Globally scss
+import './assets/scss/main.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
