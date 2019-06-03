b6c8697833f58e677bf7b3f16f72f11506ff9f9a
bjaberi <behnam.ite@gmail.com>
2019-06-04 04:10:02 +0430

    added router and vue date filter to main js file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import router from './router'
+import VueMoment from 'vue-moment'
+import moment from 'moment-timezone'
 
 // Globally scss
 import './assets/scss/main.scss'
@@ -10,8 +13,10 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
+Vue.use(VueMoment, {moment})
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
