b474cc5e8888c8a0ba67427ad91fa83dd531101f
bjaberi <behnam.ite@gmail.com>
2019-06-04 04:08:28 +0430

    created single page view

---
+++ src/views/single.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container article" v-if="mypost">
+    <img :src='mypost.author.image' :alt='mypost.title' v-if="mypost.author" />
+    <h1>{{mypost.title}}</h1>
+    <p>
+      {{mypost.body}}
+    </p>
+    <p>
+      {{mypost.description}}
+    </p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Config from '../../config/config.js'
+
+Vue.use(VueAxios, axios)
+var baseUrl = Config.baseUrl
+export default {
+  name: 'single',
+  components: {
+  },
+  data () {
+    return {
+      mypost: []
+    }
+  },
+  created () {
+    this.slug = this.$route.params.slug
+    this.getPost()
+  },
+  methods: {
+    // get post
+    getPost () {
+        axios.get(baseUrl + '/articles/' + this.slug)
+        .then(res => {
+          this.mypost = res.data.article
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+  }
+}
+</script>
+<style lang="scss">
+.article {
+  padding: 60px 0;
+  position: relative;
+
+  & img {
+    width: 100%;
+    margin-bottom: 45px;
+  }
+
+  & h1 {
+    position: absolute;
+    font-size: 25px;
+    top: 100px;
+    right: 0;
+    left: 0;
+    margin: auto;
+    color: white;
+    background: #44444430;
+    width: fit-content;
+    padding: 1% 3%;
+    border-radius: 4px;
+    text-transform: uppercase;
+  }
+}
+</style>
+
