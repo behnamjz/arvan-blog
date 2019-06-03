b000eccf257d212febf7ddf564a18a8d3a7d395a
bjaberi <behnam.ite@gmail.com>
2019-06-04 04:10:48 +0430

    created card component

---
+++ src/components/card.vue
@@ -0,0 +1,22 @@
+<template>
+	<router-link :to="'/post/'+ postdata.slug" v-if="postdata" class="col-md-6">
+        <div class="post-card">
+          <img :src="postdata.author.image" :alt='postdata.title' v-if='postdata.author.image' />
+          <div class="col-12 card-text">
+              <h2>{{postdata.title}}</h2>
+              <h4>{{postdata.createdAt | moment("dddd, MMMM Do YYYY") }}</h4>
+              <p>{{postdata.description}}</p>
+          </div>
+        </div>
+	</router-link>
+
+</template>
+<script>
+export default {
+  name: 'Card',
+  props: ['postdata', 'index'],
+  data () {
+    return {}
+  }
+}
+</script>
