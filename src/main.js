31442f51ee752882836505ef7762b03855b1893d
bjaberi <behnam.ite@gmail.com>
2019-06-05 11:39:18 +0430

    added toaster to project

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ import BootstrapVue from 'bootstrap-vue'
 import router from './router'
 import VueMoment from 'vue-moment'
 import moment from 'moment-timezone'
+import Toaster from 'v-toaster'
 
 // Globally scss
 import './assets/scss/main.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'v-toaster/dist/v-toaster.css'
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 Vue.use(VueMoment, {moment})
+Vue.use(Toaster, {timeout: 5000})
 Vue.config.productionTip = false
 
 new Vue({
