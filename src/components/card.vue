6f0509a287b4465cebd632c53b606cddd8d25911
bjaberi <behnam.ite@gmail.com>
2019-06-05 11:39:47 +0430

    edit card image status

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,7 +1,7 @@
 <template>
 	<router-link :to="'/post/'+ postdata.slug" v-if="postdata" class="col-md-6">
         <div class="post-card">
-          <img :src="postdata.author.image" :alt='postdata.title' v-if='postdata.author.image' />
+          <img :src="postdata.author.image" :alt='postdata.title' />
           <div class="col-12 card-text">
               <h2>{{postdata.title}}</h2>
               <h4>{{postdata.createdAt | moment("dddd, MMMM Do YYYY") }}</h4>
